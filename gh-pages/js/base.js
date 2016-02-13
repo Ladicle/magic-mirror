caef9e0e5f9fd744a67d873cf39a474a810809f9
Ladicle <Ladiclexxx@gmail.com>
2016-02-14 04:02:06 +0900

    connect to milkcocoa

--- gh-pages/js/base.js
+++ gh-pages/js/base.js
@@ -1,91 +1,61 @@
-var AUTH0_CLIENT_ID='uXgVqdM2nkQBzDZrrjldvIRryL8JkUPM'; 
-var AUTH0_DOMAIN='ladicle.auth0.com'; 
-var AUTH0_CALLBACK_URL=location.href;
+"use strict";
 
-$(function() {
- var userProfile;
- var lock = new Auth0Lock(
-   // All these properties are set in auth0-variables.js
-   AUTH0_CLIENT_ID,
-   AUTH0_DOMAIN
- );
+var CLIENT_ID = '714461968364-8ql5e5addflq8e0feudsr6k3humjp87d.apps.googleusercontent.com';
+var SCOPES = ["https://www.googleapis.com/auth/calendar.readonly", "profile"];
+var milkcocoa = new MilkCocoa('leadikkwvben.mlkcca.com');
+var ds = milkcocoa.dataStore('events');
 
- $('.login').click(function(e) {
-   e.preventDefault();
-   lock.showSignin(function(err, profile, token) {
-     if (err) {
-       // Error callback
-       console.log("There was an error");
-	 
-     } else {
-       // Save the JWT token.
-       localStorage.setItem('userToken', token);
-	 
-       // Save the profile
-       changeMode('detect')
-       userProfile = profile;
-       $('.nickname').text(profile.nickname);
-       $('.nickname').text(profile.name);
-     }
-   });
- });
-
- $.ajaxSetup({
-   'beforeSend': function(xhr) {
-     if (localStorage.getItem('userToken')) {
-       xhr.setRequestHeader('Authorization',
-             'Bearer ' + localStorage.getItem('userToken'));
-     }
-   }
- });
-
- $('.btn-api').click(function(e) {
-   // Just call your API here. The header will be sent
-   $.ajax({
-     url: 'http://localhost:3001/secured/ping',
-     method: 'GET'
-   }).then(function(data, textStatus, jqXHR) {
-     alert("The request to the secured enpoint was successfull");
-   }, function() {
-     alert("You need to download the server seed and start it to call this API");
-   });
- });
-});
-
-
-const milkcocoa = new MilkCocoa('leadikkwvben.mlkcca.com');
-const ds = milkcocoa.dataStore('events');
-const id = makeId();
-
-// Send Reaction
-let buttons = [...document.querySelectorAll('.reaction')];
-Bacon.fromArray(buttons)
-  .flatMap((e) => Bacon.fromEventTarget(e, 'click'))
-  .map((e) => e.target.value)
-  .onValue((value) => ds.send({to: 'pc', type: value}));
-
-// Change Mode
-let stream = Bacon.fromBinder((callback) => {
-    ds.on('send', callback);
-    () => ds.off('send');
-  })
-  .filter((data) => data.value.to == 'sp');
-
-stream
-  .map((data) => data.value.mode)
-  .onValue((mode) => {
-    changeMode(mode);
-  });
-
-function changeMode(mode){
-  document.getElementsByClassName('showing')[0].classList.remove('showing');
-  document.getElementsByClassName(mode)[0].classList.add('showing');
+function checkAuth() {
+  gapi.auth.authorize({
+    'client_id': CLIENT_ID,
+    'scope': SCOPES.join(' '),
+    'response_type': 'code token',
+    'immediate': true
+  }, handleAuthResult);
 }
 
-function makeId(){
-  let randam = Math.floor(Math.random()*1000);
-  let date = new Date();
-  let time = date.getTime();
-  return randam + time.toString();
+function handleAuthResult(authResult) {
+  gapi.client.load('oauth2', 'v2', function () {
+    gapi.client.oauth2.userinfo.get().execute(function (user) {
+      var id = user.id;
+      var email = user.email;
+      var name = user.given_name;
+      var code = authResult.code;
 
+      // Send to milkcocoa
+      ds.push({ 'user': { 'id': id, 'email': email, 'name': name, 'code': code } });
+      $('.nickname').text(name);
+    });
+  });
+
+  if (authResult && !authResult.error) {
+    // Logined
+    changeMode('detect');
+  } else {
+    // Show login buttion
+    changeMode('login');
+  }
+}
+
+function handleAuthClick() {
+  gapi.auth.authorize({ client_id: CLIENT_ID, scope: SCOPES, immediate: false }, handleAuthResult);
+  return false;
+}
+
+$(function () {
+  // Login
+  $('.login').click(function () {
+    handleAuthClick();
+  });
+
+  // Send events
+  $('.reaction').click(function () {
+    ds.push({ event: $(this).value });
+  });
+});
+
+// Change mode
+function changeMode(mode) {
+  document.getElementsByClassName('showing')[0].classList.remove('showing');
+  document.getElementsByClassName(mode)[0].classList.add('showing');
 }
