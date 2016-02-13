0fb81f9d3f5329cfd27be1c17362bbcd02ef0ec5
Ladicle <Ladiclexxx@gmail.com>
2016-02-14 01:11:53 +0900

    add google settings

--- gh-pages/js/base.js
+++ gh-pages/js/base.js
@@ -1,3 +1,58 @@
+var AUTH0_CLIENT_ID='uXgVqdM2nkQBzDZrrjldvIRryL8JkUPM'; 
+var AUTH0_DOMAIN='ladicle.auth0.com'; 
+var AUTH0_CALLBACK_URL=location.href;
+
+$(function() {
+ var userProfile;
+ var lock = new Auth0Lock(
+   // All these properties are set in auth0-variables.js
+   AUTH0_CLIENT_ID,
+   AUTH0_DOMAIN
+ );
+
+ $('.login').click(function(e) {
+   e.preventDefault();
+   lock.showSignin(function(err, profile, token) {
+     if (err) {
+       // Error callback
+       console.log("There was an error");
+	 
+     } else {
+       // Save the JWT token.
+       localStorage.setItem('userToken', token);
+	 
+       // Save the profile
+       changeMode('detect')
+       userProfile = profile;
+       $('.nickname').text(profile.nickname);
+       $('.nickname').text(profile.name);
+     }
+   });
+ });
+
+ $.ajaxSetup({
+   'beforeSend': function(xhr) {
+     if (localStorage.getItem('userToken')) {
+       xhr.setRequestHeader('Authorization',
+             'Bearer ' + localStorage.getItem('userToken'));
+     }
+   }
+ });
+
+ $('.btn-api').click(function(e) {
+   // Just call your API here. The header will be sent
+   $.ajax({
+     url: 'http://localhost:3001/secured/ping',
+     method: 'GET'
+   }).then(function(data, textStatus, jqXHR) {
+     alert("The request to the secured enpoint was successfull");
+   }, function() {
+     alert("You need to download the server seed and start it to call this API");
+   });
+ });
+});
+
+
 const milkcocoa = new MilkCocoa('leadikkwvben.mlkcca.com');
 const ds = milkcocoa.dataStore('events');
 const id = makeId();
@@ -32,5 +87,5 @@ function makeId(){
   let date = new Date();
   let time = date.getTime();
   return randam + time.toString();
-}
 
+}
